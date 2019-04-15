0b96f37ed3a2e30a300136141afbe7fc5dbdf5d8
Nelzon Rada <33723655+nkramirez@users.noreply.github.com>
2019-04-15 04:25:35 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Token da API Olho Vivo da SPTRANS
+sptrans_token = 'TOKEN_API_SPTRANS'
+
+# Números das linhas a serem monitoradas
+linhas = ['N101', '8544', '9500']
+
+# Configurações
+exibir_resultados = True
+salvar_banco_dados = True
+url_mongodb = 'mongodb://localhost:27017/'
