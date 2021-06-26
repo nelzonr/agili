d167315d8adb7f28a0f380dfb3556fcd9bbd9b1d
EC2 Default User <ec2-user@ip-172-31-35-61.us-east-2.compute.internal>
2021-06-26 05:06:45 +0000

    Linhas de onibus alteradas

---
+++ config.sample.py
@@ -0,0 +1,17 @@
+# Token da API Olho Vivo da SPTRANS
+sptrans_token = 'TOKEN_API_SPTRANS'
+
+# Números das linhas a serem monitoradas
+linhas = ['477P', '695Y', '3539']
+'''
+# 3 maiores linhas de ônibus da cidade de São Paulo #
+
+1   477P	Ipiranga – Rio Pequeno
+2   695Y	Terminal Parelheiros – Metrô Vila Mariana
+3   3539    Metrô Bresser – Cidade Tiradentes
+'''
+
+# Configurações
+exibir_resultados = True
+salvar_banco_dados = True
+url_mongodb = 'mongodb://localhost:27017/'
